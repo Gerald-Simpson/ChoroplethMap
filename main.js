c13df05e56acf45ce3262d1bd5ed0180065b68f8
simpson9955 <gtnsimpson@gmail.com>
2022-07-27 17:41:47 +0100

    setup fetch for both json links

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 /** @format */
 
-let dataSource =
+let dataSourceData =
 	"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
 
-fetch(dataSource)
+let dataSourceMap =
+	"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+
+var DataMap = {};
+
+fetch(dataSourceMap)
 	.then((data) => data.json())
-	.then((jsonData) => {
-		const w = 1000;
-		const h = 600;
+	.then((jsonDataMap) => {
+		DataMap = jsonDataMap;
+	})
+	.then(
+		fetch(dataSourceData)
+			.then((data) => data.json())
+			.then((DataData) => {
+				const w = 1000;
+				const h = 600;
 
-		const svg = d3
-			.select("#chart")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
+				const svg = d3
+					.select("#chart")
+					.append("svg")
+					.attr("width", w)
+					.attr("height", h);
 
-		svg.append("rect").attr("width", w).attr("height", h).attr("fill", "pink");
-	});
+				svg
+					.append("rect")
+					.attr("width", w)
+					.attr("height", h)
+					.attr("fill", "pink");
+			})
+	);
