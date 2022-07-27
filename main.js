965a0b804aa27654b2159684bbff56fce3482544
simpson9955 <gtnsimpson@gmail.com>
2022-07-27 17:18:34 +0100

    further init setup

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 /** @format */
+
+let dataSource =
+	"https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+
+fetch(dataSource)
+	.then((data) => data.json())
+	.then((jsonData) => {
+		const w = 1000;
+		const h = 600;
+
+		const svg = d3
+			.select("#chart")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+
+		svg.append("rect").attr("width", w).attr("height", h).attr("fill", "pink");
+	});
